bc7f069e38271be0c1067d7d8453adbd7fb8988f
SkyHuss <fl.gonzales5790@gmail.com>
2019-04-12 11:26:40 +0200

    feat: gestion des options

--- src/Entity/PropertySearch.php
+++ src/Entity/PropertySearch.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Validator\Constraints as Assert;
 
 class PropertySearch
@@ -17,6 +18,34 @@ class PropertySearch
      */
     private $minSurface;
 
+
+    /**
+     * @var ArrayCollection
+     */
+    private $options;
+
+    public function __construct()
+    {
+        $this->options = new ArrayCollection();
+    }
+
+    /**
+     * @return ArrayCollection
+     */
+    public function getOptions(): ArrayCollection
+    {
+        return $this->options;
+    }
+
+    /**
+     * @param ArrayCollection $options
+     * @return PropertySearch
+     */
+    public function setOptions(ArrayCollection $options): PropertySearch
+    {
+        $this->options = $options;
+        return $this;
+    }
     /**
      * @return int|null
      */
